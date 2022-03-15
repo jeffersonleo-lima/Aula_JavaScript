d0d81d1c863a7cf3204190f065d011fe0b20fd57
jeffersonleo-lima <99993846+jeffersonleo-lima@users.noreply.github.com>
2022-03-14 22:37:19 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+var frutas = [{nome: "maca", cor: "vermelha"}, {nome: "uva", cor: "roxa"}];
+console.log(frutas[1].nome);
+alert(frutas[1].nome);
+
+/*var fruta = {nome: "maca", cor: "vermelha"};
+console.log(fruta.nome);
+alert(fruta.cor);*/
+
+/*var lista = ["maca", "pera", "laranja"];
+lista.push("uva");
+lista.pop();
+console.log(lista.reverse());*/
+
+/*var nome = "Jefferson";
+var n1 = 36;
+var n2 = 10;
+var frase = "Japao é o Melhor Time do Mundo";
+alert(nome + " tem "+ idade + " anos");
+alert(idade + idade_nova);
+console.log(nome);
+console.log(n1 + n2);
+console.log(frase);
+console.log(frase.replace("Japao", "Brasil"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());*/
